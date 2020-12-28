8b4832f6441069d956b10050e83219abc660f384
prpaxson <prpaxson@gmail.com>
2020-12-27 21:58:26 -0800

    trying to fix theme.less error

---
+++ library.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var library = {};
+
+library.defineWidgetAreas = function(areas, callback) {
+	areas = areas.concat([
+		{
+			'name': 'Category Left Sidebar',
+			'template': 'category.tpl',
+			'location': 'lsidebar'
+		},
+		{
+			'name': 'Category Right Sidebar',
+			'template': 'category.tpl',
+			'location': 'sidebar'
+		},
+		{
+			'name': 'Topic Left Sidebar',
+			'template': 'topic.tpl',
+			'location': 'lsidebar'
+		},
+		{
+			'name': 'Topic Right Sidebar',
+			'template': 'topic.tpl',
+			'location': 'sidebar'
+		},
+		{
+			'name': 'Topic Footer',
+			'template': 'topic.tpl',
+			'location': 'footer'
+		},
+		{
+			'name': 'Tag Left Sidebar',
+			'template': 'tag.tpl',
+			'location': 'lsidebar'
+		},
+		{
+			'name': 'Tag Right Sidebar',
+			'template': 'tag.tpl',
+			'location': 'sidebar'
+		},
+	]);
+
+	callback(null, areas);
+};
+
+module.exports = library;
